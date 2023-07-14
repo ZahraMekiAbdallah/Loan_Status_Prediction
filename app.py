89de2fce455c472b8fe4b03f6ae149f2413e257a
ZahraMekiAbdallah <zahrameki18@gmail.com>
2023-07-14 12:39:58 +0300

    [ADD] img and update app file

--- app.py
+++ app.py
@@ -29,30 +29,49 @@ def prediction(Gender, Dependents, Self_Employed, Monthly_ApplicantIncome, Month
     test_df.at[0,"Monthly_LoanAmount"] = LoanAmount / Loan_Amount_Term
     test_df.at[0,"Monthly_App_LoanPercent"] = (LoanAmount / Loan_Amount_Term) / Monthly_ApplicantIncome if Monthly_ApplicantIncome > 0 else 0
     test_df.at[0,"Monthly_Coapp_LoanPercent"] = (LoanAmount / Loan_Amount_Term) / Monthly_CoapplicantIncome if Monthly_CoapplicantIncome > 0 else 0
-    st.dataframe(test_df)
-#     for col in Inputs:
-#         if col not in ['Gender', 'Property_Area', 'Is_Graduated']:
-#             test_df[col] = test_df[col].astype('float64')
-#     test_df['Is_Graduated'] = test_df['Is_Graduated'].astype('int64')
+#     st.dataframe(test_df)
+
     result = final_model.predict(test_df)
     result_prop = final_model.predict_proba(test_df)[0][1] * 100
-    print(result,'--------------------',result_prop)
     return result, result_prop
 
     
 def main():
-#     st.image('indian_airlines.jpg')
-    Gender = st.selectbox("Gender" , ['Male', 'Female'])
-    Dependents = st.selectbox("No of Dependents" , [0, 1, 2, 3])
-    Self_Employed = st.checkbox("Self Employed ?")    
-    Is_Married = st.checkbox("Married ?")    
-    Is_Graduated = st.checkbox("Graduated ?") 
-    Credit_History = st.checkbox("Positive Credit History?")
-    Property_Area = st.selectbox("Property Area" , ['Semiurban', 'Urban', 'Rural'])
-    Monthly_ApplicantIncome = st.slider("Applicant Income (Month)", 150, 30000)
-    Monthly_CoapplicantIncome = st.slider("Co-Applicant Income (Month)", 0, 10000)
-    LoanAmount = st.slider("Loan Amount", 9000, 300000)
-    Loan_Amount_Term = st.selectbox("Loan Amount Term (Month)" , [12, 36, 60, 84, 120, 180, 240, 300, 360, 480])
+    st.image('LoanImg.jpg')
+    dic = {'No':0, 'Yes':1}
+    c_dic = {'Negative':0, 'Positive':1}
+    d_dic = {'0':0, '1':1, '2':2,'3+':3}
+    
+    dep_cols=st.columns(2)
+    with dep_cols[0]:
+        Gender = st.selectbox("Gender" , ['Male', 'Female'])
+    with dep_cols[1]:
+        Dependents = d_dic[st.selectbox("No of Dependents" , ['0', '1', '2', '3+'])]
+    
+    dep_cols=st.columns(2)
+    with dep_cols[0]:
+        Credit_History = c_dic[st.radio("Credit History ?", ('Negative', 'Positive'))]
+
+    with dep_cols[1]:
+        Self_Employed = dic[st.radio("Self Employed ?", ('No', 'Yes'))]
+
+        
+    dep_cols=st.columns(2)
+    with dep_cols[0]:
+        Is_Graduated = dic[st.radio("Graduated ?", ('No', 'Yes'))]
+    with dep_cols[1]:
+        Is_Married = dic[st.radio("Married ?", ('No', 'Yes'))]
+    
+    Monthly_ApplicantIncome = st.slider("Applicant Income (Month)", 150, 50000)
+    Monthly_CoapplicantIncome = st.slider("Co-Applicant Income (Month)", 0, 50000)
+    LoanAmount = st.slider("Loan Amount", 1000, 1000000)
+    
+    dep_cols=st.columns(2)
+    with dep_cols[0]:
+        Property_Area = st.selectbox("Property Area" , ['Semiurban', 'Urban', 'Rural'])
+    with dep_cols[1]:
+        Loan_Amount_Term = st.selectbox("Loan Amount Term (Month)" , [12, 36, 60, 84, 120, 180, 240, 300, 360, 480])
+
     
     if st.button("Predict", type='primary'):
         result, result_prop = prediction(Gender, Dependents, Self_Employed, Monthly_ApplicantIncome, Monthly_CoapplicantIncome,
